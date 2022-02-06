63a6afc1332742ff4556535c1e3b4768609a5e74
tinyakov.ru <a@tinyakov.ru>
2022-02-07 02:19:39 +0300

    home work4

--- src/main/scala/module1/App.scala
+++ src/main/scala/module1/App.scala
@@ -5,7 +5,10 @@ object App {
 
   def main(args: Array[String]): Unit = {
 
-      functional_effects.functionalProgram.declarativeEncoding.interpret(functional_effects.functionalProgram.declarativeEncoding.p1)
+//      functional_effects.functionalProgram.declarativeEncoding.interpret(functional_effects.functionalProgram.declarativeEncoding.p1)
+    module2.homework_hkt_impllicts.r1
+    module2.homework_hkt_impllicts.r2
+
   }
 
 }
